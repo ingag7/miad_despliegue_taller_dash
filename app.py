afb664565ab1e84d6375e640ca606d4b076be3f0
ingag7 <c.galindoa2@uniandes.edu.co>
2024-10-17 21:22:50 -0500

    nueva función de carga de datos

--- app.py
+++ app.py
@@ -21,7 +21,16 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    df = pd.read_csv('//Users/ag7/Documents/MIAD/Ciclo 5 - 202415/Despliegue de Soluciones/Week 1/taller_tablero/datos_energia.csv')
+    
+    # Convertir la columna de fecha al tipo datetime
+    df['time'] = pd.to_datetime(df['time'])
+    
+    # Establecer la columna de fecha como índice del DataFrame
+    df.set_index('time', inplace=True)
+    
+    # Retornar el DataFrame
+    return df
     
 
 # Cargar datos
